c1ee43241ad5c36558d411ac32fb6c93b3307b67
Elyzaveta Kharkevych <y.kharkevych@ukma.edu.ua>
2026-06-04 13:54:55 +0300

    feat: add project structure and TypeScript types

---
+++ frontend/src/types/item.ts
@@ -0,0 +1,41 @@
+import { type PublicProfileResponse } from './user'
+
+export type ItemCondition = 'IDEAL' | 'GOOD' | 'NORM' | 'BAD' | 'NEEDS_REPAIRING'
+export type RentingStatus = 'AVAILABLE' | 'RENTED' | 'HIDDEN' | 'ARCHIVED'
+
+export interface ItemRequest {
+    categoryId: number
+    title: string
+    description: string | null
+    tags: string[]
+    condition: ItemCondition
+    pricePerDay: number
+    pricePerWeek: number | null
+    depositAmount: number
+    city: string
+    address: string
+    latitude: number
+    longitude: number
+}
+
+export interface ItemResponse {
+    id: number
+    ownerId: number 
+    ownerProfile: PublicProfileResponse
+    categoryId: number
+    title: string
+    description: string | null
+    tags: string[]
+    condition: ItemCondition
+    pricePerDay: number
+    pricePerWeek: number | null
+    depositAmount: number
+    status: RentingStatus
+    city: string
+    address: string
+    latitude: number
+    longitude: number
+    isVerified: boolean
+    createdAt: string
+    updatedAt: string
+}
