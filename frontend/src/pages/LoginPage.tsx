02462f88cb0a1e507e64a566f726ca9502d5b1ce
Elyzaveta Kharkevych <y.kharkevych@ukma.edu.ua>
2026-06-05 16:08:53 +0300

    feat: implement login page logic and form

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,56 @@
+import {useState} from "react";
+import {useNavigate} from "react-router";
+import {useDispatch} from "react-redux";
+import {setCredentials} from "../store/slices/authSlice.ts";
+
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState('')
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try {
+        const { user, token, refreshToken } = await login({ email, password });
+        dispatch(setCredentials({ user, token, refreshToken }));
+        navigate("/");
+    } catch (error) {
+      setError("Помилка входу. Перевірте email та пароль.");
+    }
+  };
+
   return (
     <div>
       <h1>Вхід</h1>
+      <form onSubmit={handleSubmit}>
+        {error && <p>{error}</p>}
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            placeholder="Введіть email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Пароль:</label>
+          <input
+            type="password"
+            placeholder="Введіть пароль"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Вхід</button>
+      </form>
     </div>
   );
 }
 
 export default LoginPage    
+
+
